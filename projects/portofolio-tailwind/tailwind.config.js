4aa632ce7c56150d2807a5408576fe4fdf606012
alhiefikri <alhiepedia@gmail.com>
2024-10-18 15:57:26 +0800

    menambahkan fitur darkmode

--- projects/portofolio-tailwind/tailwind.config.js
+++ projects/portofolio-tailwind/tailwind.config.js
@@ -1,19 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
-      padding: "16px",
+      padding: '16px',
     },
     extend: {
       colors: {
-        primary: "#14b8a6",
-        secondary: "#64748b",
-        dark: "#0f172a",
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#0f172a',
       },
       screens: {
-        "2xl": "1320px",
+        '2xl': '1320px',
       },
     },
   },
